d27a42dffdf50f75cffcf13f48b1c3c6942b06c6
Amir Hossein Asghari <amir.hossein.asghari.x@gmail.com>
2025-10-28 16:48:30 +0330

    feat: updated top-navbar and added banner for the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import TopNavbar from "@/components/top-navbar";
 import HeroSection from "@/components/hero-section";
 import Footer from "@/components/footer";
+import Banner from "@/components/ui/banner";
 
 export default function Home() {
   return (
       <div className="flex flex-col w-full">
           <TopNavbar/>
+          <Banner/>
           <HeroSection/>
           <Footer/>
       </div>
--- src/components/top-navbar.tsx
+++ src/components/top-navbar.tsx
@@ -1,19 +1,163 @@
 "use client"
 
 import {Button} from "@/components/ui/button";
-import { ChevronDown } from 'lucide-react';
+import {ChevronDown, ChevronUp} from 'lucide-react';
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import {HugeiconsIcon} from '@hugeicons/react';
+import {ArrowLeft01Icon, AiProgrammingIcon, CheckmarkSquare02Icon} from '@hugeicons/core-free-icons';
+import {AnimatePresence, motion} from "motion/react";
+import {cn} from "@/lib/utils"
+import {ReactNode, useState} from "react";
+import Link from "next/link";
+
+interface CardForCoursesProps {
+    onMouseEnter?: () => void,
+    active?: boolean
+}
+
+function CardForCourses({onMouseEnter, active}: CardForCoursesProps) {
+    return (
+        <div onMouseEnter={onMouseEnter}
+             className={cn("w-[380px] h-[120px] hover:bg-black/80 hover:text-white hover:border-gray-800 duration-150 border-gray-500 border rounded-[10px] flex justify-center items-center cursor-pointer", active && "bg-black/80 text-white")}>
+            <div className="w-[350px] h-[90px] flex flex-col gap-[10px]">
+                <div className="w-full h-[52px] flex justify-between">
+
+                    <div className="w-[52px] h-[52px] flex justify-center items-center">
+                        <HugeiconsIcon
+                            icon={ArrowLeft01Icon}
+                            size={42}
+                            color="currentColor"
+                            strokeWidth={1.5}
+                        />
+                    </div>
+
+                    <div className="h-full  flex gap-[20px] justify-end">
+                        <div className="h-full flex items-center font-semibold text-[20px] select-none">دوره های
+                            آموزشی
+                        </div>
+                        <div className="w-[52px] h-full flex justify-center items-center ">
+                            <HugeiconsIcon
+                                icon={AiProgrammingIcon}
+                                size={42}
+                                color="currentColor"
+                                strokeWidth={1.5}
+                            />
+                        </div>
+                    </div>
+
+                </div>
+                <div className="w-[350px] h-[28px] flex justify-end select-none">
+                    ورود به دنیای حرفه ای تکنولوژی
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+interface CourseInfoBannerProps {
+    children: ReactNode;
+}
+
+const modalVariants = {
+    visible: {opacity: 1, transition: {when: "beforeChildren"}, duration: 1},
+    hidden: {opacity: 0, transition: {when: "afterChildren"}, duration: 1},
+}
+
+
+function CourseInfoBanner({children}: CourseInfoBannerProps) {
+    return (
+        <motion.div
+            className="w-[648px] h-[570px] rounded-[10px]"
+            variants={modalVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+        >
+            <div className="h-[570px] flex flex-col box-content justify-between items-end">
+                <div className="flex flex-col gap-[30px] items-end mt-[30px]">
+                    <span className="text-4xl font-extrabold">دوره های تخصصی</span>
+                    <span className="text-xl text text-black">ورود به دنیای حرفه ای تکنولوژی</span>
+                </div>
+                <div className="flex flex-col">
+                    <div className="flex flex-row-reverse items-center gap-2.5 h-[40px]">
+                        <HugeiconsIcon icon={CheckmarkSquare02Icon}
+                                       color={"#196B00"}
+                                       fill={"#BFFFA3"}
+                                       strokeWidth={1.5}/>
+                        <div>آموزش همه تخصصی</div>
+                    </div>
+                    <div className="flex flex-row-reverse  items-center gap-2.5 h-[40px]">
+                        <HugeiconsIcon icon={CheckmarkSquare02Icon}
+                                       color={"#196B00"}
+                                       fill={"#BFFFA3"}
+                                       strokeWidth={1.5}/>
+                        <div>یادگیری جمعی</div>
+                    </div>
+                    <div className="flex flex-row-reverse items-center gap-2.5 h-[40px]">
+                        <HugeiconsIcon icon={CheckmarkSquare02Icon}
+                                       color={"#196B00"}
+                                       fill={"#BFFFA3"}
+                                       strokeWidth={1.5}/>
+                        <div>منتورشیپ</div>
+                    </div>
+                </div>
+                <div className="flex ml-[30px] mb-[30px] self-start">
+                    <Link className="w-[220px] h-[70px] flex justify-center items-center  text-xl bg-black text-white font-bold hover:bg-white hover:text-[#FF8E50] hover:border-[#FF8E50] hover:border-4 duration-200  rounded-[10px]" href="#">شروع یادگیری</Link>
+                </div>
+            </div>
+        </motion.div>
+    );
+}
+
+function PopoverInnerContent() {
+    const [show, setShow] = useState(1);
+
+    return (
+        <div className="w-full h-full flex justify-between items-center">
+            <AnimatePresence mode="wait">
+                {show === 1 && <CourseInfoBanner key="1">Content 1</CourseInfoBanner>}
+                {show === 2 && <CourseInfoBanner key="2">Content 2</CourseInfoBanner>}
+                {show === 3 && <CourseInfoBanner key="3">Content 3</CourseInfoBanner>}
+                {show === 4 && <CourseInfoBanner key="4">Content 4</CourseInfoBanner>}
+            </AnimatePresence>
+            <span className="w-[1px] h-[340px] bg-black/20"></span>
+            <div className="w-[380px] h-[570px] rounded-[10px] flex flex-col gap-[30px]">
+                {/*Card component for course type*/}
+                <CardForCourses onMouseEnter={() => setShow(1)} active={show === 1}/>
+                <CardForCourses onMouseEnter={() => setShow(2)} active={show === 2}/>
+                <CardForCourses onMouseEnter={() => setShow(3)} active={show === 3}/>
+                <CardForCourses onMouseEnter={() => setShow(4)} active={show === 4}/>
+            </div>
+        </div>
+    );
+};
 
 export default function TopNavbar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="flex justify-between items-center h-[100px] w-full">
-            <div className="w-[265px] h-fit">
-                <Button className="m-5 h-[60px] w-[225px]">ورود / عضویت</Button>
+            <div className="w-[265px] h-full flex justify-center items-center">
+                <Link className="m-5 h-[60px] w-[225px] flex justify-center items-center bg-[#FF8E50] text-black font-bold hover:bg-black hover:text-[#FF8E50] hover:border-[#FF8E50] hover:border-4 duration-200  rounded-[10px]" href="/auth">ورود / عضویت</Link>
             </div>
             <div className="w-[265px] h-fit  flex justify-center items-center">
-                <div className="flex flex-col w-full justify-center items-center">
-                    <h1>دوره های آموزشی</h1>
-                    <ChevronDown />
-                </div>
+                <Popover open={isOpen} onOpenChange={setIsOpen}>
+                    <PopoverTrigger asChild>
+                        <div className="flex flex-col w-full justify-center items-center cursor-pointer"
+                             onClick={() => setIsOpen(!isOpen)}>
+                            <h1 className="select-none">دوره های آموزشی</h1>
+                            {isOpen ? (
+                                    <ChevronUp/>
+                                ) :
+                                <ChevronDown/>
+                            }
+                        </div>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[1158px] h-[600px] bg-white/50 backdrop-blur-2xl">
+                        <PopoverInnerContent/>
+                    </PopoverContent>
+                </Popover>
+
             </div>
             <div className="w-[265px] h-fit ">
                 <Button className="m-5 h-[60px] w-[225px]">LOGO</Button>
