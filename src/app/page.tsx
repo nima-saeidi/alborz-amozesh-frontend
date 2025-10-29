2853bc8462beb420c5b0e3a9ad53835f37c60a7d
Amir Hossein Asghari <amir.hossein.asghari.x@gmail.com>
2025-10-29 17:58:19 +0330

    feat: updated banner component and added props to it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Banner from "@/components/ui/banner";
 export default function Home() {
   return (
       <>
-          <Banner/>
+          <Banner title={"شروع مسیر تازه یادگیری"} description={"با آموزش های تخصصی قدم بعدی رو در زندگی و کار بردار "} linkAvailable={true} linkTitle="شروع یادگیری" link="/courses"/>
           <HeroSection/>
       </>
   );
--- src/components/ui/banner.tsx
+++ src/components/ui/banner.tsx
@@ -1,5 +1,13 @@
 import Link from "next/link";
-export default function Banner() {
+
+interface BannerProps {
+    title: string;
+    description: string;
+    linkAvailable: boolean;
+    linkTitle?: string;
+    link?: string;
+}
+export default function Banner({ title, description, linkAvailable, linkTitle, link }: BannerProps) {
 
     return (
         <div className="h-[350px] relative mx-5 mt-[30px] rounded-[10px] bg-[#FF8E50] overflow-hidden select-none">
@@ -7,10 +15,10 @@ export default function Banner() {
                 <div className="size-[260px] ml-[100px] my-[45px]"></div>
                 <div className="h-[250px] mr-[50px] w-1/2 flex flex-col items-end justify-between ">
                     <div className="flex flex-col w-full items-end gap-[40px]">
-                        <div className="text-5xl font-semibold text-white">شروع مسیر تازه یادگیری</div>
-                        <div className="text-2xl font-semibold text-white/80">با آموزش های تخصصی قدم بعدی رو در زندگی و کار بردار </div>
+                        <div className="text-5xl font-semibold text-white">{title}</div>
+                        <div className="text-2xl font-semibold text-white/80">{description}</div>
                     </div>
-                    <Link className="w-[220px] h-[70px] flex justify-center items-center text-xl bg-black text-white font-bold hover:bg-white hover:text-[#FF8E50] hover:border-[#FF8E50] hover:border-4 duration-200  rounded-[10px]" href="#">شروع یادگیری</Link>
+                    {linkAvailable && (<Link className="w-[220px] h-[70px] flex justify-center items-center text-xl bg-black text-white font-bold hover:bg-white hover:text-[#FF8E50] hover:border-[#FF8E50] hover:border-4 duration-200  rounded-[10px]" href={link ? link : ""}>{linkTitle}</Link>)}
                 </div>
             </div>
         </div>
