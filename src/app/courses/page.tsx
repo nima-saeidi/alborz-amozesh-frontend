7d89adcb5d3b56aa67af69b656119af1b6a52e74
Amir Hossein Asghari <amir.hossein.asghari.x@gmail.com>
2025-12-02 05:28:05 +0330

    chore: changed courses filter options

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -16,10 +16,7 @@ export default function CoursePage() {
         <>
             <Banner title={"آموزش با چاشنی مهارت"} description={"یادگیری با کیفیت و منتورشیب نارنجی"} linkAvailable={false}/>
             <div className="relative lg:mx-5 mt-[50px] overflow-hidden flex flex-col">
-                <div className="h-[50px] flex justify-between items-center border-b">
-                    <div className="h-full">
-                        <SearchBar />
-                    </div>
+                <div className="h-[50px] flex-row-reverse justify-between items-center border-b">
                     <div className="h-full flex flex-row-reverse gap">
                         <div className="w-[100px] border-b-4 border-black flex justify-center items-center">ویدیویی</div>
                         <div className="w-[100px] flex justify-center items-center">حضوری</div>
